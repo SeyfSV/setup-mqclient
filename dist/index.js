63d5d26acb558d64818cf6bfd872f559e01e1b6b
Seyf SV <25712100+SeyfSV@users.noreply.github.com>
2020-02-04 20:49:25 +0300

    dev

--- dist/index.js
+++ dist/index.js
@@ -8404,54 +8404,52 @@ module.exports = __webpack_require__(890)
 
 const core = __webpack_require__(470)
 const io = __webpack_require__(1)
-
 const fs  = __webpack_require__(747);
 
-const RDURL = 'https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist'
-const RDURL_MAC = 'https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/mactoolkit'
-const RDTAR = 'IBM-MQC-Redist-LinuxX64.tar.gz'
-const RDZIP = 'IBM-MQC-Redist-Win64.zip'
-const MTTAR = 'IBM-MQ-Toolkit-MacX64.tar.gz'
+const REDIST_URL_LNX = 'https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist'
+const REDIST_URL_WIN = REDIST_URL_LNX
+const TOOLKIT_URL_MAC = 'https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/mactoolkit'
+const ARCHIVE_LNX = 'IBM-MQC-Redist-LinuxX64.tar.gz'
+const ARCHIVE_WIN = 'IBM-MQC-Redist-Win64.zip'
+const ARCHIVE_MAC = 'IBM-MQ-Toolkit-MacX64.tar.gz'
 
-const PATH_LINUX = '/opt/mqm'
+const PATH_LINUX = '~/IBM/MQ'
 const PATH_WIN = 'c:/Program Files (x86)/IBM/WebSphere MQ'
-const PATH_MAC = '/Users/runner/mqm' //Does not work with ~/mqm
+const PATH_MAC = '~/IBM/MQ'
+const PATH_DATA_MAC = '~/IBM/MQ/data'
 
-const VRMF = core.getInput('mq-client-version')
-const INST_PATH = core.getInput('mq-file-path')
+const MQ_CLIENT_VERSION = core.getInput('mq-client-version')
+const PATH_INST = core.getInput('mq-file-path')
 
 var os = __webpack_require__(87);
 var platform = os.platform();
 var file_name;
-var mq_file_path
-var url
+var mq_file_path = PATH_INST;
+var url;
 
 switch (platform)
 {
     case "linux":
-        url = RDURL
-        file_name = VRMF + '-' + RDTAR
-        if (INST_PATH == '')
+        url = REDIST_URL_LNX
+        file_name = MQ_CLIENT_VERSION + '-' + ARCHIVE_LNX
+        if (PATH_INST == '')
             mq_file_path = PATH_LINUX
-        else
-            mq_file_path = INST_PATH
         break;
     case "win32":
-        url = RDURL
-        file_name = VRMF + '-' + RDZIP
-        if (INST_PATH == '')
+        url = REDIST_URL_LNX
+        file_name = MQ_CLIENT_VERSION + '-' + ARCHIVE_WIN
+        if (PATH_INST == '')
             mq_file_path = PATH_WIN
-        else
-            mq_file_path = INST_PATH
         break;
     case "darwin":
-        url = RDURL_MAC
-        file_name = VRMF + '-' + MTTAR
-        if (INST_PATH == '')
+        url = TOOLKIT_URL_MAC
+        file_name = MQ_CLIENT_VERSION + '-' + ARCHIVE_MAC
+        if (PATH_INST == '')
             mq_file_path = PATH_MAC
-        else
-            mq_file_path = INST_PATH
         break;
+    default:
+        core.setFailed('Platform ' + platform + ' is unknown!')
+        process.exit(1)
 }
 
 process.env.MQ_FILE_PATH = mq_file_path
@@ -8463,7 +8461,15 @@ if (!fs.existsSync(mq_file_path)){
     core.info('Created dir ' + mq_file_path)
 }
 
-// core.log('Downloading MQ Client')
+if(platform == 'darwin'){
+    if (process.env.MQ_OVERRIDE_DATA_PATH){
+        fs.mkdirSync(process.env.MQ_OVERRIDE_DATA_PATH, {recursive: true});
+    }
+    else{
+        fs.mkdirSync(PATH_DATA_MAC, {recursive: true});
+        
+    }
+}
 
 const https = __webpack_require__(211);
 const decompress = __webpack_require__(820);
@@ -8472,8 +8478,6 @@ const unzip = __webpack_require__(942)
 const file = fs.createWriteStream(mq_file_path + '/' + file_name)
 const request = https.get(url + '/' + file_name,
     (res) => {
-        core.info('statusCode:', res.statusCode);
-
         res.on('data', (d) => {
             file.write(d);
         });
@@ -8484,10 +8488,12 @@ const request = https.get(url + '/' + file_name,
                 res.on('end', () => {
                     console.log('MQ Client downloaded')
                     console.log('Extracting archive "' + file_name + '" ...')
-                    fs.createReadStream(mq_file_path + '/' + file_name)
-                        .pipe(unzip.Extract({ path:  mq_file_path }));
-                    console.log('Extracted')
+                    // fs.createReadStream(mq_file_path + '/' + file_name)
+                    //     .pipe(unzip.Extract({ path:  mq_file_path }));
+                    decompress(mq_file_path + '/' + file_name, mq_file_path).then(files=> {
+                        core.info('Extracted')
                     })
+                })
                 break
             case "linux":
                 res.on('end', () => {
@@ -8496,9 +8502,12 @@ const request = https.get(url + '/' + file_name,
                     decompress(mq_file_path + '/' + file_name, mq_file_path).then(files=> {
                         core.info('Extracted')
                         if (process.env.LD_LIBRARY_PATH)
-                            core.exportVariable('LD_LIBRARY_PATH', `${mq_file_path}/lib64:${process.env['LD_LIBRARY_PATH']}`)
+                            lib_path = `${mq_file_path}/lib64:${process.env['LD_LIBRARY_PATH']}`
                         else
-                            core.exportVariable('LD_LIBRARY_PATH', `${mq_file_path}/lib64`)
+                            lib_path = `${mq_file_path}/lib64`
+                        core.exportVariable('LD_LIBRARY_PATH', lib_path)
+                        core.exportVariable('mq-lib-var', `LD_LIBRARY_PATH`)
+                        core.exportVariable('mq-lib-path', `${mq_file_path}/lib64`)
                     })
                 })
                 break
@@ -8508,14 +8517,15 @@ const request = https.get(url + '/' + file_name,
                     core.info('Extracting archive "' + file_name + '" ...')
                     decompress(mq_file_path + '/' + file_name, mq_file_path).then(files=> {
                         core.info('Extracted')
-                        const { exec } = __webpack_require__(129)
 
-                        if (process.env.LD_LIBRARY_PATH){
-                            core.exportVariable('LD_LIBRARY_PATH', `${mq_file_path}/lib64:${process.env['LD_LIBRARY_PATH']}`)
+                        if (process.env.DYLD_LIBRARY_PATH){
+                            lib_path = `${mq_file_path}/lib64:${process.env['DYLD_LIBRARY_PATH']}`
                         }
                         else {
-                            core.exportVariable('LD_LIBRARY_PATH', `${mq_file_path}/lib64`)
+                            lib_path = `${mq_file_path}/lib64`
                         }
+                        core.exportVariable('DYLD_LIBRARY_PATH', lib_path)
+                        core.exportVariable('mq-lib-var', `DYLD_LIBRARY_PATH`)
                         core.exportVariable('mq-lib-path', `${mq_file_path}/lib64`)
                     })
                 })
@@ -8530,9 +8540,6 @@ request.on('error', (e) => {
 )
 
 request.end()
-// file.end()
-
-
 
 /***/ }),
 
