2ae09814d91e0549d4d864e61316546a9af017c5
Seyf SV <25712100+SeyfSV@users.noreply.github.com>
2020-02-08 13:46:54 +0300

    Added support for cache

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const core = require('@actions/core')
-const io = require('@actions/io')
 const fs  = require('fs');
+const path = require('path')
+const tempDirectory = require('temp-dir');
+const expandHomeDir = require('expand-home-dir')
+const decompress = require('decompress');
 
 const REDIST_URL_LNX = 'https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist'
 const REDIST_URL_WIN = REDIST_URL_LNX
@@ -9,13 +12,20 @@ const ARCHIVE_LNX = 'IBM-MQC-Redist-LinuxX64.tar.gz'
 const ARCHIVE_WIN = 'IBM-MQC-Redist-Win64.zip'
 const ARCHIVE_MAC = 'IBM-MQ-Toolkit-MacX64.tar.gz'
 
-const PATH_LINUX = '~/IBM/MQ'
-const PATH_WIN = 'c:/Program Files (x86)/IBM/WebSphere MQ'
-const PATH_MAC = '~/IBM/MQ'
-const PATH_DATA_MAC = '~/IBM/MQ/data'
+const PATH_LINUX = expandHomeDir('~/IBM/MQ')
+const PATH_WIN = path.normalize('c:/Program Files (x86)/IBM/WebSphere MQ')
+const PATH_MAC = expandHomeDir('~/IBM/MQ')
+const PATH_DATA_MAC = expandHomeDir('~/IBM/MQ/data')
 
 const MQ_CLIENT_VERSION = core.getInput('mq-client-version')
 const PATH_INST = core.getInput('mq-file-path')
+const FORCE_DWNLD = (core.getInput('force-download') === 'true')
+
+const DWNLD_PATH = core.getInput('download-path')
+if (!fs.existsSync(DWNLD_PATH))
+    fs.mkdirSync(DWNLD_PATH, {recursive: true});
+dwnld_path = path.resolve(DWNLD_PATH);
+core.debug(`Download directory path is ${dwnld_path}`)
 
 var os = require('os');
 var platform = os.platform();
@@ -32,7 +42,7 @@ switch (platform)
             mq_file_path = PATH_LINUX
         break;
     case "win32":
-        url = REDIST_URL_LNX
+        url = REDIST_URL_WIN
         file_name = MQ_CLIENT_VERSION + '-' + ARCHIVE_WIN
         if (PATH_INST == '')
             mq_file_path = PATH_WIN
@@ -67,72 +77,84 @@ if(platform == 'darwin'){
     }
 }
 
-const https = require('https');
-const decompress = require('decompress');
-const unzip = require('node-unzip-2')
+var dwnld_archive_path = path.join(dwnld_path, file_name)
+core.debug(`Archive exists: ${fs.existsSync(dwnld_archive_path)}`)
+core.debug(`Force download: ${FORCE_DWNLD}`)
+if (fs.existsSync(dwnld_archive_path)
+        && fs.statSync(dwnld_archive_path)['size'] > 0
+        && !FORCE_DWNLD){
+    core.info(`Extracting archive "${file_name}" to "${mq_file_path}" from cache...`)
+    extract(dwnld_archive_path, mq_file_path)
+    setup_variables()
+}
+else{
+    core.info('Downloading MQ Client...')
+    const https = require('https');
+    var temporary_archive_path = path.join(tempDirectory, file_name);
+    const file = fs.createWriteStream(temporary_archive_path);
 
-const file = fs.createWriteStream(mq_file_path + '/' + file_name)
-const request = https.get(url + '/' + file_name,
+    let request = https.get(url + '/' + file_name,
     (res) => {
-        res.on('data', (d) => {
-            file.write(d);
+        res.pipe(file).on('close', () => {
+            core.info('Downloaded');
+            core.debug(`Archive size: ${fs.statSync(temporary_archive_path)['size']}`);
+            core.debug(`Copy archive from "${temporary_archive_path}" to "${dwnld_archive_path}"`);
+            fs.copyFileSync(temporary_archive_path, dwnld_archive_path);
+            extract(dwnld_archive_path, mq_file_path);
+            setup_variables();
         });
-
-        switch(platform)
-        {
-            case "win32":
-                res.on('end', () => {
-                    console.log('MQ Client downloaded')
-                    console.log('Extracting archive "' + file_name + '" ...')
-                    // fs.createReadStream(mq_file_path + '/' + file_name)
-                    //     .pipe(unzip.Extract({ path:  mq_file_path }));
-                    decompress(mq_file_path + '/' + file_name, mq_file_path).then(files=> {
-                        core.info('Extracted')
-                    })
-                })
-                break
-            case "linux":
-                res.on('end', () => {
-                    core.info('MQ Client downloaded')
-                    core.info('Extracting archive "' + file_name + '" ...')
-                    decompress(mq_file_path + '/' + file_name, mq_file_path).then(files=> {
-                        core.info('Extracted')
-                        if (process.env.LD_LIBRARY_PATH)
-                            lib_path = `${mq_file_path}/lib64:${process.env['LD_LIBRARY_PATH']}`
-                        else
-                            lib_path = `${mq_file_path}/lib64`
-                        core.exportVariable('LD_LIBRARY_PATH', lib_path)
-                        core.exportVariable('mq-lib-var', `LD_LIBRARY_PATH`)
-                        core.exportVariable('mq-lib-path', `${mq_file_path}/lib64`)
-                    })
-                })
-                break
-            case "darwin":
-                res.on('end', () => {
-                    core.info('MQ Client downloaded')
-                    core.info('Extracting archive "' + file_name + '" ...')
-                    decompress(mq_file_path + '/' + file_name, mq_file_path).then(files=> {
-                        core.info('Extracted')
-
-                        if (process.env.DYLD_LIBRARY_PATH){
-                            lib_path = `${mq_file_path}/lib64:${process.env['DYLD_LIBRARY_PATH']}`
-                        }
-                        else {
-                            lib_path = `${mq_file_path}/lib64`
-                        }
-                        core.exportVariable('DYLD_LIBRARY_PATH', lib_path)
-                        core.exportVariable('mq-lib-var', `DYLD_LIBRARY_PATH`)
-                        core.exportVariable('mq-lib-path', `${mq_file_path}/lib64`)
-                    })
-                })
-                break
-        }
-        core.addPath(mq_file_path + '/bin');
-        core.addPath(mq_file_path + '/bin64');
+        res.on('error', (error)=>{
+            core.setFailed(error.message)
+        });
     });
+    request.end();
+}
 
-request.on('error', (e) => {
-    core.error(e.message)}
-)
 
-request.end()
+function setup_variables(){
+    switch(platform)
+    {
+        case "linux":
+            if (process.env.LD_LIBRARY_PATH)
+                lib_path = `${mq_file_path}/lib64:${process.env['LD_LIBRARY_PATH']}`
+            else
+                lib_path = `${mq_file_path}/lib64`
+            core.exportVariable('LD_LIBRARY_PATH', lib_path)
+            core.exportVariable('mq-lib-var', `LD_LIBRARY_PATH`)
+            core.exportVariable('mq-lib-path', `${mq_file_path}/lib64`)
+            break
+        case "darwin":
+            if (process.env.DYLD_LIBRARY_PATH){
+                lib_path = `${mq_file_path}/lib64:${process.env['DYLD_LIBRARY_PATH']}`
+            }
+            else {
+                lib_path = `${mq_file_path}/lib64`
+            }
+            core.exportVariable('DYLD_LIBRARY_PATH', lib_path)
+            core.exportVariable('mq-lib-var', `DYLD_LIBRARY_PATH`)
+            core.exportVariable('mq-lib-path', `${mq_file_path}/lib64`)
+            break
+    }
+    core.addPath(path.join(mq_file_path, '/bin'));
+    core.addPath(path.join(mq_file_path, '/bin64'));
+
+}
+
+function extract(input, output){
+    core.debug(`Archive path: ${input}`)
+    core.debug(`Archive size: ${fs.statSync(input)['size']}`)
+    
+    core.info(`Extracting archive "${input}" to "${output}" ...`);
+    decompress(input, output).then(
+        files=> {
+            core.info(`Archive extracted!`)
+        },
+        error=>{
+            core.setFailed(error.message)
+        })
+        .catch(
+            error=>{
+                throw new Error(`Error occured!: ${error}`) 
+            }
+        )
+}
